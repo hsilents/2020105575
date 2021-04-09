975fe5df85d925fb12ed1c88165076ed53a74956
“ <1024968781@qq.com>
2021-04-09 11:18:21 +0900

    1~8 times table

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main(){
+	int i, j;
+	cout << "구구단:\n";
+	for (i = 1; i < 9; i++) {
+		for (j = 1; j < 9; j++) {
+			cout << i << "*" << j << "=" << i * j << '\t';
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
